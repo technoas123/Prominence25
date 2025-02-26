15cb9781aeefc752e1a5c457384e92392224466f
ahammed <ahammedsalahuddeenaffiliate@gmail.com>
2025-02-26 12:14:00 +0530

    Updated events data

--- src/data/events.js
+++ src/data/events.js
@@ -4,6 +4,7 @@ const events = [
         title: "Prominence",
         description:
           "The annual tech fest of ECE & Robotics, RIT Kottayam, is back with a bang!",
+        content: "🚀 Get Ready for Prominence 25! 🚀 The annual tech fest of ECE & Robotics, RIT Kottayam, is back with a bang! Prominence 25 is set to ignite innovation, challenge minds, and showcase cutting-edge technology. Stay tuned for thrilling competitions, workshops, and tech exhibitions that will push the boundaries of creativity.",
         image: "/images/prominrnce_announcement.jpg",
         date: "March 06, 2025",
         location: "Main Auditorium",
@@ -14,6 +15,7 @@ const events = [
         title: "Prominence Launch",
         description:
           "The Department of ECE & Robotics presents Prominence 2025, the festival of innovation and creativity where ideas becomes reality",
+          content: "For a succesful technology, reality must take precedence over public relations, for Nature cannot be fooled. The Department of ECE & Robotics presents Prominence 2025, the festival of innovation and creativity where ideas becomes reality. Discover innovative ideas, connect with passionate minds, and create unforgettable experiences✨. Join us on 6th and 7th of March 2025. Save the dates and stay tuned for more updates...!",
         image: "/images/proinence_launch.jpg",
         date: "March 06, 2025",
         location: "Main Auditorium",
@@ -24,50 +26,55 @@ const events = [
         title: "Bonic Expo",
         description:
           "Get ready for the ultimate tech and innovation showcase at PROMINENCE 2025",
+        content: "The Department of ECE & ROBOTICS presents Bonic Expo,a project expo competition in association with Bonic.ai  🏣Venue: RIT KOTTAYAM  📅Date: 06/03/2025 💰Exciting prizes up for grabs, including a free ticket to Dubai. Register here 🔴Registration is free and open to all.",
         image: "/images/bonic_expo.jpg",
         date: "March 06, 2025",
         location: "Main Auditorium",
-        link: "#",
+        link: "https://forms.gle/rhu2QiCWwoenyah58",
       },
       {
         id: 4,
         title: "Gerena FreeFire Tournament",
         description:
           "The Gerena FreeFire Tournament, powered by Prominence'25 in collaboration with Gamers Spot RIT, is here to test your skills and crown the ultimate champion.",
+        content: "The Garena FreeFire Tournament, powered by Prominence'25 in collaboration with Gamers Spot RIT, is here to test your skills and crown the ultimate champion. 🔸️Date: 25/02/2025 🔸️Team Structure: 4 Members  🔸️Game Mode: Battle Royale 🔸️Registration Fee: ₹80 per team  Win a full manthi feast to crown your victory 🏆 Sign up now🚀",
         image: "/images/event3.jpg",
         date: "February 25, 2025",
         location: "Online",
-        link: "#",
+        link: "https://forms.gle/ytdn3Y7tGSBBg2dGA",
       },
       {
         id: 5,
         title: "PUBG Tournament",
         description:
           "Take aim, stay sharp, and battle your way to the top—Welcome to PUBG Tournament💣",
+        content:"Take aim, stay sharp, and battle your way to the top—Welcome to  Player Unknown BattleGround Tournament💣.Hosted by Prominence'25 in collaboration with Gamers Spot RIT. 🔸️Date: 26/02/2025 🔸️Team Structure: 4 Members  🔸️Game Mode: Team Death Match 🔸️Registration Fee: ₹50 per team Win a full manthi feast to crown your victory 🏆",
         image: "/images/event4.jpg",
         date: "February 26, 2025",
         location: "Online",
-        link: "#",
+        link: "https://forms.gle/Cda6K6YyHwUUXiALA",
       },
       {
         id: 6,
         title: "Mini Miltia Tournament",
         description:
           "Unleash Mayhem, Ignite Strategy Welcome to the World of Mini Militia, Face off with top players, prove your skills, and get your hands on some epic prizes",
+        content:"Unleash Mayhem, Ignite Strategy Welcome to the World of Mini Militia🥷Join the Mini Miltia Tournament, brought to you by Prominence'25 in partnership with Gamers Spot RIT. 🔸️Date: 27/02/2025 🔸️Team Structure: 4 Members  🔸️Mode: Offline @ College Auditorium  🔸️Registration Fee: ₹40 per team Win a full manthi feast to crown your victory 🏆",
         image: "/images/event5.jpg",
         date: "February 27, 2025",
         location: "College Auditorium",
-        link: "#",
+        link: "https://forms.gle/hUb2p1fBN4Wtg2qm6",
       },
       {
         id: 7,
         title: "e-Football Tournament",
         description:
           "Kick off your journey to virtual football glory in the e-Football Championship ⚽.Hosted by Prominence'25 and Gamers Spot RIT.",
+        content:"Kick off your journey to virtual football glory in the e-Football Championship ⚽.Hosted by Prominence'25 and Gamers Spot RIT. 🔸️Date: 28/02/2025 🔸️Game Mode: Online 🔸️Registration Fee: ₹30 per head Win a full manthi feast to crown your victory 🏆",
         image: "/images/event6.jpg",
         date: "February 28, 2025",
         location: "Online",
-        link: "#",
+        link: "https://forms.gle/H1g9W3m99WQp82gU8",
       },
 
   ];
